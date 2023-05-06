0f780eeed148c6998e4c86dba91e91852507bd24
Mizzzean <129473207+Mizzzean@users.noreply.github.com>
2023-05-05 20:05:27 -0700

    Add files via upload

--- The-Office-Data-Viz.R
+++ The-Office-Data-Viz.R
@@ -8,24 +8,40 @@ library("ggplot2")
 # https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/TheOfficeIMDBPerEpisode.csv
 # This data is drawn from Kaggle: https://www.kaggle.com/datasets/kapastor/the-office-imdb-ratings-per-episode
 
-office_df <-
+office_df <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/TheOfficeIMDBPerEpisode.csv")
 # convert column to date format
 office_df$AirDate<- as.Date(office_df$AirDate)
 
 # What is the episode(s) with the highest IMDB rating in the dataset?
 # Filter the data and save the row(s) as highest_rating_rows
-Your code here
+highest_rating <- max(office_df$Rating)
+highest_rating_rows <- filter(office_df, Rating == highest_rating)
 
 # What is the episode(s) with the lowest IMDB rating in the dataset?
 # Filter the data and save the row(s) as lowest_rating_rows
+lowest_rating <- min(office_df$Rating)
+lowest_rating_rows <- filter(office_df, Rating == lowest_rating)
 
 # Which season of The Office was the best?
 # Calculate the average IMDB rating for *each season*
 # Save as rating_per_season
-Your code here
+rating_per_season <- office_df %>%
+  group_by(Season) %>%
+  summarize(avg_rating = mean(Rating))
+
 
 # Plot the IMDB rating for every episode of The Office as a line plot
 # with date on the X axis and IMDB rating on the Y axis
+ggplot(office_df, aes(x = AirDate, y = Rating)) +
+  geom_line()
 
 # Now plot the *average* IMDB rating for *each season* as a scatterplot, a line plot, and both
-Your code here
+ggplot(rating_per_season, aes(x = Season, y = avg_rating)) +
+  geom_point()
+
+ggplot(rating_per_season, aes(x = Season, y = avg_rating)) +
+  geom_line()
+
+ggplot(rating_per_season, aes(x = Season, y = avg_rating)) +
+  geom_point() +
+  geom_line()
--- Wealth-Gap.R
+++ Wealth-Gap.R
@@ -5,7 +5,10 @@ home_owner <- read.csv('https://raw.githubusercontent.com/rfordatascience/tidytu
 race_wealth <- read.csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/race_wealth.csv')
 
 # Load relevant libraries
-Your code here
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
 
 # How is average family wealth in the U.S. trending over time?
 
@@ -13,27 +16,62 @@ Your code here
 # Then we need to make a new dataframe from race_wealth with the average family wealth per year for all races
 # Save this as `total_avg_wealth`
 
-Your code here
+total_avg_wealth <- race_wealth %>%
+  filter(type == "Average") %>%
+  group_by(year) %>%
+  summarise(avg_wealth = mean(wealth_family, na.rm = TRUE))
 
 # Now let's make a scatter plot of total average family wealth over time
 
-Your code here
+
+ggplot(total_avg_wealth, aes(x = year, y = avg_wealth)) +
+  geom_point() +
+  labs(title = "Total Average Family Wealth over Time",
+       x = "Year",
+       y = "Average Family Wealth")
+
 
 # Now let's draw a line plot on top of the same scatterplot
 
-Your code here
+ggplot(total_avg_wealth, aes(x = year, y = avg_wealth)) +
+  geom_point() +
+  geom_line() +
+  labs(title = "Total Average Family Wealth over Time",
+       x = "Year",
+       y = "Average Family Wealth")
+
 
 # Let's plot family wealth over time by racial category
 
 # Use the race_wealth dataframe and filter for only "Average" wealth `avg_race_wealth`
 
-Your code here
+avg_race_wealth <- race_wealth %>%
+  filter(type == "Average")
+
+ggplot(avg_race_wealth, aes(x = year, y = wealth_family)) +
+  geom_point() +
+  facet_wrap(~ race) +
+  labs(title = "Family Wealth over Time by Racial Category",
+       x = "Year",
+       y = "Average Family Wealth")
+
 
 # Let's make a line plot of average family wealth over time by race
 
-Your code here
+ggplot(avg_race_wealth, aes(x = year, y = wealth_family, color = race)) +
+  geom_line() +
+  labs(title = "Average Family Wealth over Time by Racial Category",
+       x = "Year",
+       y = "Average Family Wealth")
+
 
 # Finally, let's make a line plot of the percentage of homeowners for racial categories over time, and color by race
 # Use the home_owner dataframe
 
-Your code here
+ggplot(home_owner, aes(x = year, y = home_owner_pct, color = race)) +
+  geom_line() +
+  labs(title = "Percentage of Homeowners by Racial Category over Time",
+       x = "Year",
+       y = "Percentage of Homeowners")
+
+
